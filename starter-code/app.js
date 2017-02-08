5808f80af2704f56951180903c6e95836c34a7c6
SarahNox <apnoeist@googlemail.com>
2017-02-08 17:46:56 +0100

    initial commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,4 +2,51 @@ const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
+const expressLayouts = require('express-ejs-layouts');
 
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('port 3000 in use')
+});
+
+app.get('/random', (request, response) => {
+   client.getRandomJoke().then(function (res) {
+      jokeValue = res.value;
+        response.render("index", {joke: jokeValue});
+    }).catch(function (err) {
+        console.log("what are you doing here?");
+    });
+});
+
+app.get('/categories', (request, response) => {
+  client.getJokeCategories().then(function (res) {
+      response.render("categories", {categories: res})
+  }).catch(function (err) {
+      console.log("no categories found");
+  });
+});
+
+// not working
+app.get('/categories/:category', (request, response) => {
+  response.send(`Chuck Norris Jokes in the category: ${request.params.category}`)
+    client.getRandomJoke().then(function (res) {
+      jokeValue = res.value;
+      console.log(jokeValue);
+          response.render("joke-by-category", {jokeCategory: jokeValue});
+      }).catch(function (err) {
+          console.log("no, that's not working");
+    })
+});
+
+// Free text search
+// app.get('/search', (request, response) => {
+// client.search(searchTerm).then(function (response) {
+//     // to stuff here
+// }).catch(function (err) {
+//     console.log("not found");
+// });
+// });
